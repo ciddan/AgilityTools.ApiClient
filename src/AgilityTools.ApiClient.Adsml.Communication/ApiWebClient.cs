84353410eb17b6d52518e4c4d4a213e239a1c793
Mikael Selander <ciddan@gmail.com>
2010-11-05 00:24:24 +0100

    Added UploadDataAsync to IApiWebClient and ApiWebClient.

--- src/AgilityTools.ApiClient.Adsml.Communication/ApiWebClient.cs
+++ src/AgilityTools.ApiClient.Adsml.Communication/ApiWebClient.cs
@@ -29,8 +29,29 @@ namespace AgilityTools.ApiClient.Adsml.Communication
             return _webClient.UploadData(url, method, data);
         }
 
-        public void Dispose()
-        {
+        public void UploadDataAsync(string url, string method, byte[] data, Action<byte[]> callback) {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            if (callback == null) {
+                throw new ArgumentNullException("callback");
+            }
+
+            if (string.IsNullOrEmpty(url))
+                throw new InvalidOperationException("Url must be provided.");
+
+            if (string.IsNullOrEmpty(method))
+                throw new InvalidOperationException("A method must be provided.");
+
+            var uri = new Uri(url, UriKind.Absolute);
+
+            _webClient.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+
+            _webClient.DownloadDataCompleted += (sender, args) => callback.Invoke(args.Result);
+            _webClient.UploadDataAsync(uri, method, data);
+        }
+
+        public void Dispose() {
             _webClient.Dispose();
         }
     }
