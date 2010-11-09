430237a20cae1530405d540d90d68abbea192dd4
Mikael Selander <mikael.selander@jula.se>
2010-11-09 10:26:31 +0100

    Preparing merge of SearchControlBuilder

---
+++ src/AgilityTools.ApiClient.Adsml.Client/Components/LanguageToReturn.cs
@@ -0,0 +1,29 @@
+using System.Xml.Linq;
+
+namespace AgilityTools.ApiClient.Adsml.Client
+{
+    public class LanguageToReturn : IReturnedLanguageControl
+    {
+        private readonly int _langId;
+
+        internal LanguageToReturn(int langId) {
+            _langId = langId;
+        }
+
+        public XElement ToAdsml() {
+            this.Validate();
+
+            return new XElement("Language", new XAttribute("id", _langId.ToString()));
+        }
+
+        public void Validate() {
+            if (_langId <= 0) {
+                throw new ApiSerializationValidationException("LanguageId cannot be negative or 0.");
+            }
+        }
+
+        public static LanguageToReturn WithLanguageId(int langId) {
+            return new LanguageToReturn(langId);
+        }
+    }
+}
