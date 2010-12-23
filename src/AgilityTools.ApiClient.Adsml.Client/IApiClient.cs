2d0c2e65bb997d7979afb2752920ad3ffb2a63cc
Mikael Selander <mikael.selander@jula.se>
2010-12-23 15:23:12 +0100

    Added possible signatures for SendApiRequest/Async with typed responses in IApiClient.

--- src/AgilityTools.ApiClient.Adsml.Client/IApiClient.cs
+++ src/AgilityTools.ApiClient.Adsml.Client/IApiClient.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Xml.Linq;
+using AgilityTools.ApiClient.Adsml.Client.Responses.Converters;
 
 namespace AgilityTools.ApiClient.Adsml.Client
 {
@@ -7,5 +8,13 @@ namespace AgilityTools.ApiClient.Adsml.Client
     {
         XElement SendApiRequest<TRequest>(TRequest request) where TRequest : class, IAdsmlSerializable<XElement>;
         void SendApiRequestAsync<TRequest>(TRequest request, Action<XElement> callback) where TRequest : class, IAdsmlSerializable<XElement>;
+
+        TOutput SendApiRequest<TRequest, TOutput>(TRequest request, IResponseConverter<XElement, TOutput> converter)
+            where TRequest : class, IAdsmlSerializable<XElement>
+            where TOutput : class;
+
+        void SendApiRequestAsync<TRequest, TOutput>(TRequest request, IResponseConverter<XElement, TOutput> converter, Action<TOutput> callback) 
+            where TRequest : class, IAdsmlSerializable<XElement>
+            where TOutput : class;
     }
 }
