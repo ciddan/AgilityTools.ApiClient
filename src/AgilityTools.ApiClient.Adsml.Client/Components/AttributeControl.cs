8cc1c1579bf86b7cdb1080e7652d623d725e35ed
Mikael Selander <selander@axpa.se>
2013-03-13 16:01:16 +0100

    Spacing cleanup

--- src/AgilityTools.ApiClient.Adsml.Client/Components/AttributeControl.cs
+++ src/AgilityTools.ApiClient.Adsml.Client/Components/AttributeControl.cs
@@ -4,39 +4,39 @@ using System.Xml.Linq;
 
 namespace AgilityTools.ApiClient.Adsml.Client.Components
 {
+  /// <summary>
+  /// Allows the user to control the returned attributes.
+  /// </summary>
+  public class AttributeControl : ControlComponentBase<IAttributeControl>, ISearchControlComponent, ILookupControlComponent
+  {
     /// <summary>
-    /// Allows the user to control the returned attributes.
+    /// Constructor. Internal.
     /// </summary>
-    public class AttributeControl : ControlComponentBase<IAttributeControl>, ISearchControlComponent, ILookupControlComponent
-    {
-        /// <summary>
-        /// Constructor. Internal.
-        /// </summary>
-        /// <param name="attributesToReturn">A param array of <see cref="IAttributeControl"/> containing the attributes to return.</param>
-        internal AttributeControl(params IAttributeControl[] attributesToReturn)
-            : this("AttributesToReturn", attributesToReturn) {
-        }
-
-        /// <summary>
-        /// Constructor. Internal.
-        /// </summary>
-        /// <param name="nodeName">Optional. The name of the wrapping xml tag. Defaults to AttributesToReturn.</param>
-        /// <param name="attributesToReturn">A param array of <see cref="IAttributeControl"/> containing the attributes to return.</param>
-        internal AttributeControl(string nodeName = "AttributesToReturn", params IAttributeControl[] attributesToReturn) {
-            if (attributesToReturn == null)
-                throw new ArgumentNullException("attributesToReturn");
-
-            this.OuterNodeAttributes = new List<XAttribute>();
-            this.ContentNodes = new List<IAttributeControl>(attributesToReturn);
-            this.NodeName = nodeName;
-        }
-
-        /// <summary>
-        /// Adds attributes to the return list.
-        /// </summary>
-        /// <param name="attributesToReturn">A param array of <see cref="IAttributeControl"/> containing the attributes to return.</param>
-        internal void AddAttributes(params IAttributeControl[] attributesToReturn) {
-            this.ContentNodes = new List<IAttributeControl>(attributesToReturn);
-        }
+    /// <param name="attributesToReturn">A param array of <see cref="IAttributeControl"/> containing the attributes to return.</param>
+    internal AttributeControl(params IAttributeControl[] attributesToReturn)
+      : this("AttributesToReturn", attributesToReturn) {
     }
+
+    /// <summary>
+    /// Constructor. Internal.
+    /// </summary>
+    /// <param name="nodeName">Optional. The name of the wrapping xml tag. Defaults to AttributesToReturn.</param>
+    /// <param name="attributesToReturn">A param array of <see cref="IAttributeControl"/> containing the attributes to return.</param>
+    internal AttributeControl(string nodeName = "AttributesToReturn", params IAttributeControl[] attributesToReturn) {
+      if (attributesToReturn == null)
+        throw new ArgumentNullException("attributesToReturn");
+
+      this.OuterNodeAttributes = new List<XAttribute>();
+      this.ContentNodes = new List<IAttributeControl>(attributesToReturn);
+      this.NodeName = nodeName;
+    }
+
+    /// <summary>
+    /// Adds attributes to the return list.
+    /// </summary>
+    /// <param name="attributesToReturn">A param array of <see cref="IAttributeControl"/> containing the attributes to return.</param>
+    internal void AddAttributes(params IAttributeControl[] attributesToReturn) {
+      this.ContentNodes = new List<IAttributeControl>(attributesToReturn);
+    }
+  }
 }
