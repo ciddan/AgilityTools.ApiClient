74422912eaf5a25749b4abf8fa47378601328d2d
Mikael Selander <ciddan@gmail.com>
2013-09-28 17:05:01 +0200

    Added filters for the SimpleSearchRequest.

--- src/AgilityTools.ApiClient.Adsml.Client/Requests/SimpleSearchRequest.cs
+++ src/AgilityTools.ApiClient.Adsml.Client/Requests/SimpleSearchRequest.cs
@@ -1,17 +1,23 @@
 using System;
+using System.Linq;
 using System.Xml.Linq;
+using System.Collections.Generic;
 using AgilityTools.ApiClient.Adsml.Client.Components;
 
 namespace AgilityTools.ApiClient.Adsml.Client
 {
   public class SimpleSearchRequest : IAdsmlSerializable<XElement>
   {
+    public IList<ISearchRequestFilter> RequestFilters { get; private set; }
+
     private readonly string baseContext;
     private readonly SearchControl searchControls;
 
     public SimpleSearchRequest(string baseContext) : this(baseContext, null) {}
 
     public SimpleSearchRequest(string baseContext, SearchControl searchControls) {
+      this.RequestFilters = new List<ISearchRequestFilter>();
+
       if (baseContext == null)
         throw new ArgumentNullException("baseContext");
       
@@ -27,6 +33,10 @@ namespace AgilityTools.ApiClient.Adsml.Client
       
       if (this.searchControls != null) {
         request.AddFirst(this.searchControls.ToAdsml());
+      }
+
+      if (this.RequestFilters.Count > 0) {
+        request.Add(this.RequestFilters.Select(f => f.ToAdsml()));
       }
       
       return request;
